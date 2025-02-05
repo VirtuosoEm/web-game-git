a157af2e86dccc39e99c68e91c67e1862e81b1a1
VirtuosoEm <57539776+VirtuosoEm@users.noreply.github.com>
2025-02-05 15:38:53 +0300

    UpdateV1.1.4

--- WebGameGitHub/Assets/Scripts/SelectItems.cs
+++ WebGameGitHub/Assets/Scripts/SelectItems.cs
@@ -6,9 +6,17 @@ using UnityEngine;
 
 public class SelectItems : MonoBehaviour
 {
+    [SerializeField] private int _money;
+    [SerializeField] private TextMeshProUGUI _moneyText;
+    private const string MONEY_TAG = "Money";
+
+
     private Transform _itemParent;
+    [SerializeField] private List<Item> _items;
 
     [SerializeField] private TextMeshProUGUI _selectText;
+    [SerializeField] private TextMeshProUGUI _buyButtonText;
+    [SerializeField] private GameObject _buyButton;
 
     [SerializeField] private string _key;
     private int _currentIndex;
@@ -16,7 +24,15 @@ public class SelectItems : MonoBehaviour
 
     private void Start()
     {
+        _money = PlayerPrefs.HasKey(MONEY_TAG) ? PlayerPrefs.GetInt(MONEY_TAG) : 1000;
+        _moneyText.text = $"Денег: {_money}$";
+
+
         _itemParent = GetComponent<Transform>();
+        _items[0].IsPurchased = true;
+
+        for (int i = 1; i < _items.Count; i++)
+            _items[i].InitializeItem();
 
         for (int i = 0; i < _itemParent.childCount; i++)
             _itemParent.GetChild(i).gameObject.SetActive(false);
@@ -25,7 +41,7 @@ public class SelectItems : MonoBehaviour
         _currentIndex = _savedItemIndex;
 
         _itemParent.GetChild(_savedItemIndex).gameObject.SetActive(true);
-        _selectText.text = _savedItemIndex == _currentIndex ? "Выбрано" : "Выбрать";
+        UpdateItem();
 
     }
 
@@ -43,7 +59,7 @@ public class SelectItems : MonoBehaviour
         }
 
         _itemParent.GetChild(_currentIndex).gameObject.SetActive(true);
-        _selectText.text = _savedItemIndex == _currentIndex ? "Выбрано" : "Выбрать";
+        UpdateItem();
     }
 
     public void SelectRight()
@@ -59,7 +75,34 @@ public class SelectItems : MonoBehaviour
         }
 
         _itemParent.GetChild(_currentIndex).gameObject.SetActive(true);
+        UpdateItem();
+    }
+
+    private void UpdateItem()
+    {
         _selectText.text = _savedItemIndex == _currentIndex ? "Выбрано" : "Выбрать";
+
+        if (_currentIndex < _items.Count)
+        {
+            _buyButton.SetActive(_items[_currentIndex].IsPurchased == false);
+            _buyButtonText.text = $"Цена: {_items[_currentIndex].Cost}$";
+        }
+        else
+            _buyButton.SetActive(false);
+    }
+
+    public void BuyItem()
+    {
+        if (_money >= _items[_currentIndex].Cost)
+        {
+            _money -= _items[_currentIndex].Cost;
+            _moneyText.text = $"Денег: {_money}$";
+
+            _buyButton.SetActive(false);
+            _items[_currentIndex].SavePurchase();
+            SaveItem();
+            PlayerPrefs.SetInt(MONEY_TAG, _money);
+        }
     }
 
     public void SaveItem()
@@ -71,5 +114,28 @@ public class SelectItems : MonoBehaviour
         _selectText.text = "Выбрано";
     }
 
+    public void DeleteSave()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
 
 }
+[System.Serializable]
+public class Item 
+{
+    [SerializeField] private string _key = "item";
+
+    public bool IsPurchased = false;
+    public int Cost;
+
+    public void InitializeItem() =>
+        IsPurchased = PlayerPrefs.HasKey(_key) ? PlayerPrefs.GetInt(_key) == 1 : false;
+    
+    public void SavePurchase()
+    {
+        PlayerPrefs.SetInt(_key, 1);
+        IsPurchased = true;
+    }
+}
+
